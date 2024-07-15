528accc027f96acc8de7251a5f9f612f192502ac
Gabriel Cumare Montilla <gacm95218@gmail.com>
2024-07-15 02:40:27 +0000

    Created model for star wars items

--- src/models.py
+++ src/models.py
@@ -1,13 +1,13 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+""" class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
@@ -28,5 +28,47 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+ """
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False, unique=True)
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    is_banned = Column(Boolean, default=False)
+
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(250), nullable=False, unique=True)
+    description = Column(String(500), nullable=False)
+    image_url = Column(String(250))
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(250), nullable=False, unique=True)
+    description = Column(String(500), nullable=False)
+    image_url = Column(String(250))
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    character = relationship(Character)
+    planet = relationship(Planet)
+    user = relationship(User)
+
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+try:
+    render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except:
+    print("There was a problem genering the diagram")
+    raise Exception
